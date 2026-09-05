fb13d2f98a387e57e07bc5f826b7d7c9030e9e01
samuelsantosr360 <samuelsantosr360@gmail.com>
2026-09-05 12:32:52 -0300

    feat: add basic arithmetic operations module

--- testPY/app.py
+++ testPY/app.py
@@ -1,15 +1,11 @@
 def Soma (a, b):
-    variavel_nao_usada = 100  # SonarQube: Code Smell (Unused variable)
     return a + b
-    print("Este codigo nunca sera executado")  # SonarQube: Bug/Code Smell (Unreachable code)
 
 def Subtracao (a, b):
-    if a == a:  # SonarQube: Bug (Identical expressions on both sides of comparison)
-        return a - b
+    return a - b
 
 def Multiplicacao (a, b):
     return a * b
 
 def Divisao (a, b):
     return a / b
-
